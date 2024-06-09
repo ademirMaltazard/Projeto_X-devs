33da37adae15b6a77f841da194d01793e6ab05ee
ademirMaltazard <ademir.mk98@gmail.com>
2024-06-09 09:45:38 -0300

    criação do arquivo de sripts em js

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+
+
+//pegar o elemento
+const cartao = document.getElementById('btn-avancar')
+
+cartao.addEventListener("click", function(){
+    console.log("cliquei");
+})
+
+// fazer o clique
+// avançar cartao
+// ocultar cartao anterior
