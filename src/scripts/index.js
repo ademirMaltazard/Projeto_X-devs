dd07ccaf3602933862140f59121efbc452535fd5
ademirMaltazard <ademir.mk98@gmail.com>
2024-06-09 12:36:01 -0300

    finalização do script e refatoração

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,34 @@
 
 
 //pegar o elemento
-const cartao = document.getElementById('btn-avancar')
-
-cartao.addEventListener("click", function(){
-    console.log("cliquei");
-})
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
 
 // fazer o clique
-// avançar cartao
-// ocultar cartao anterior
+btnAvancar.addEventListener("click", function(){
+    const ehPrimeiroCartao = cartaoAtual === cartoes.length - 1
+    if (ehPrimeiroCartao) return;
+    EsconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao();
+});
+
+btnVoltar.addEventListener("click", function(){
+    const ehUltimoCartao = cartaoAtual === 0
+    if (ehUltimoCartao) return;
+    EsconderCartaoSelecionado();
+    cartaoAtual--;
+    mostrarCartao();
+
+});
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+function EsconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
